052c1ec1d1e9522e038812f87cf764305b37b0d5
yanxuewen <yanxuewen325@163.com>
2017-03-16 22:12:55 +0800

    init

---
+++ YXMPPDemo/YXMPPDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YXMPPDemo
+//
+//  Created by yanxuewen on 2017/3/16.
+//  Copyright © 2017年 YXW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YXMPPDemo/YXMPPDemo/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  YXMPPDemo
+//
+//  Created by yanxuewen on 2017/3/16.
+//  Copyright © 2017年 YXW. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <XMPPFramework/XMPPFramework.h>
+#import <YYCache/YYCache.h>
+#import <YYModel/YYModel.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    YYCache *cache = [[YYCache alloc] initWithName:@"ssaskjaccjf"];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
